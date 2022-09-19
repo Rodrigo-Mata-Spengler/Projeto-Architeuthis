4972dde76f1fa66ed46056543e606a1d07ec36d9
Rodrigo Mata <rodrigomataspengler@gmail.com>
2022-09-19 18:03:19 -0300

    animação do cacetete
    criada animação e codigo para ter o ataque corpo a corpo junta a defesa

--- Assets/Armas/Cacetete/Cacetete.cs
+++ Assets/Armas/Cacetete/Cacetete.cs
@@ -6,9 +6,13 @@ public class Cacetete : MonoBehaviour
 {
     [SerializeField] private Animator pupet;
 
+    [SerializeField] private float dano;
+
     private void Start()
     {
         pupet.ResetTrigger("Marretar");
+        pupet.ResetTrigger("Fire2");
+        pupet.ResetTrigger("soltar");
     }
 
     private void Update()
@@ -16,6 +20,23 @@ public class Cacetete : MonoBehaviour
         if(Input.GetButton("Fire1"))
         {
             pupet.SetTrigger("Marretar");
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            pupet.SetTrigger("Defender");
+        }else if (Input.GetButtonUp("Fire2"))
+        {
+            pupet.SetTrigger("soltar");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Health vida = collision.transform.GetComponent<Health>();
+            vida.DamageHealth(dano);
         }
     }
 }
