c3e0ab8ac829caebb2c593a19b44333233460cfb
Rodrigo Mata <rodrigomataspengler@gmail.com>
2022-11-01 22:50:21 -0300

    animações e codigo do npc

---
+++ Assets/Npc/Enemy cacetete/arma/Cacetete2.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cacetete2 : MonoBehaviour
+{
+    [SerializeField] private Animator pupet;
+
+    [SerializeField] private float dano;
+
+    [SerializeField] private GameObject escudo;
+    private void Start()
+    {
+        escudo.SetActive(false);
+        pupet.ResetTrigger("Atacar");
+        pupet.ResetTrigger("Defender");
+        pupet.ResetTrigger("Defender 2");
+    }
+
+    public void Atacar()
+    {
+        pupet.SetTrigger("Atacar");
+    }
+
+    public void Defender()
+    {
+        pupet.SetTrigger("Defender");
+        escudo.SetActive(true);
+    }
+
+    public void soltar()
+    {
+        pupet.SetTrigger("Defender 2");
+        escudo.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Health vida = collision.transform.GetComponent<Health>();
+            vida.DamageHealth(dano);
+        }
+    }
+}
