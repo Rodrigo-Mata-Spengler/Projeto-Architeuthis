f43bf9cd6400817ba33711050d88398902a6c873
Rodrigo Mata <rodrigomataspengler@gmail.com>
2022-11-13 16:11:06 -0300

    boss dash

--- Assets/Npc/Boss/BossCtrl.cs
+++ Assets/Npc/Boss/BossCtrl.cs
@@ -5,23 +5,92 @@ using UnityEngine.AI;
 
 public class BossCtrl : MonoBehaviour
 {
+    private enum Behavior {Follow,Atack,Dash,Defend};
+
+    [SerializeField] private Behavior bossMode;
+
     public NavMeshAgent boss;
 
     public Transform alvo;
 
     public bool teste;
 
+    [SerializeField] private float distanciaDeAtaque;
+
+    [Header("Dash")]
+    [SerializeField] private float dashForce;
+    [SerializeField] private float dashTime;
+
     private void Start()
     {
         boss = gameObject.GetComponent<NavMeshAgent>();
+
+        //bossMode = Behavior.Follow;
     }
 
     private void Update()
     {
-        if (!alvo)
+        //DesisionMaker();
+
+        switch (bossMode)
         {
-            boss.SetDestination(alvo.position);
+            case Behavior.Follow:
+                FollowPlayer();
+                break;
+            case Behavior.Atack:
+                break;
+            case Behavior.Dash:
+                Dash();
+                break;
+            case Behavior.Defend:
+                break;
+            default:
+                break;
         }
+    }
+
+    private void DesisionMaker()
+    {
+        if (Vector3.Distance(transform.position,alvo.position) > distanciaDeAtaque)
+        {
+            bossMode = Behavior.Follow;
+        }
+        else
+        {
+            bossMode = Behavior.Dash;
+        }
+    }
+
+    private void FollowPlayer()
+    {
+        boss.SetDestination(alvo.position);
+    }
+
+    private void Dash()
+    {
+
+        boss.ResetPath();
+        /*float direc = Random.Range(-2, 2);
+
+        gameObject.GetComponent<Rigidbody>().AddForce(dashForce * Time.deltaTime * (2 * transform.right));
+        */
+
+        StartCoroutine(DashTimer());
         
+        
+    }
+
+    IEnumerator DashTimer()
+    {
+        float starttime = Time.time;
+
+        while (Time.time < starttime + dashTime)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(dashForce * Time.deltaTime * (1 * transform.right));
+            yield return null;
+        }
+
+        bossMode = Behavior.Follow;
+        yield break;
     }
 }
